1a5fc680f51ae9d5585fb1677641e98ecd0c44c6
Rajat Kumar <rajatkumar199731@gmail.com>
2025-04-30 00:22:59 +0530

    import worker in index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,8 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 import bodyParser from 'body-parser';
 import emailRoutes from "./routes/emailRoutes";
-
-
+import "./jobs/emailProcessor";
 
 const app = express();
 
