e74040ccaee1a7aa4efb2f03f88f8a00ea09babe
Rajat Kumar <rajatkumar199731@gmail.com>
2025-04-30 18:17:58 +0530

    take port number from env in index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use("/emails", emailRoutes);
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
