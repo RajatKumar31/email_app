91198a8440597c8f04059b278e52116f864926f7
Rajat Kumar <rajatkumar199731@gmail.com>
2025-04-30 17:55:44 +0530

    Disable past date and time in time picker

--- frontend/src/components/ui/datetimepicker.tsx
+++ frontend/src/components/ui/datetimepicker.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
@@ -13,6 +13,17 @@ export const DateTimePicker: React.FC<DateTimePickerProps> = ({
   value,
   onChangeAction,
 }) => {
+  const now = new Date();
+  const isToday = value?.toDateString() === now.toDateString();
+  const minTime = isToday ? now : new Date(new Date().setHours(0, 0, 0, 0));
+  const maxTime = new Date(new Date().setHours(23, 45, 0, 0));
+
+  useEffect(() => {
+    if (!value) {
+      onChangeAction(new Date());
+    }
+  }, [value, onChangeAction]);
+
   return (
     <div className="w-full">
       <DatePicker
@@ -24,6 +35,9 @@ export const DateTimePicker: React.FC<DateTimePickerProps> = ({
         dateFormat="MMMM d, yyyy h:mm aa"
         placeholderText="Select date and time"
         className="mt-2 w-full p-2 border rounded-md shadow-sm"
+        minDate={now}
+        minTime={minTime}
+        maxTime={maxTime}
       />
     </div>
   );
