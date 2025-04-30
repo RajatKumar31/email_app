54e74c507f58593b77c8e89e94745b60a0612561
Rajat Kumar <rajatkumar199731@gmail.com>
2025-04-30 16:59:08 +0530

    remove padding in page.tsx

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -11,7 +11,7 @@ export default function HomePage() {
   return (
     <main className="flex h-screen">
       <Sidebar onComposeAction={() => setShowComposer(true)} />
-      <div className="flex-1 overflow-y-auto p-6 bg-gray-50">
+      <div className="flex-1 overflow-y-auto bg-gray-50">
         <EmailList />
       </div>
       {showComposer && (
