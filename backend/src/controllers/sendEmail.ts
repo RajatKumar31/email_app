2582540ade0d1681e0413a4e8fc74c1960d08bb2
Rajat Kumar <rajatkumar199731@gmail.com>
2025-04-30 14:19:15 +0530

    modify send email code

--- backend/src/controllers/sendEmail.ts
+++ backend/src/controllers/sendEmail.ts
@@ -1,30 +1,21 @@
 import { Request, Response } from 'express';
-import { resend } from '../config/resend';
 import { db } from '../config/db';
 import { emails } from '../schema/email';
-import { EmailStatus } from '../schema/email';
+import { emailQueue } from '../jobs/index';
 
 export async function sendNow(req: Request, res: Response): Promise<any> {
     const { to, subject, body } = req.body;
     try {
-        // Send email using resend service
-        const result = await resend.emails.send({
-            to,
-            from: 'onboarding@resend.dev',
-            subject,
-            html: body,
-        });
-
-        let status: EmailStatus = result.data?.id ? "sent" : "failed";
-
-        // Insert email record into the database
+        // Insert email into DB (with status 'pending')
         const insertedEmail = await db.insert(emails).values({
             to,
             subject,
             body,
-            status,
-            sentAt: new Date(),
+            status: 'pending',
         }).returning();
+
+        // Push to queue
+        await emailQueue.add('send-email', { email: insertedEmail[0] }, { attempts: 3 });
 
         // Return the inserted email record as a response
         return res.status(200).json(insertedEmail);
