d7c72ff1ab0d5568ae008a80ee81250cbc8fbd36
Rajat Kumar <rajatkumar199731@gmail.com>
2025-05-01 21:18:58 +0530

    remove console.log from fetchEmails

--- frontend/src/services/fetchEmails.ts
+++ frontend/src/services/fetchEmails.ts
@@ -1,11 +1,9 @@
 import axios from "axios";
 
 export const fetchEmails = async (page: number, limit: number) => {
-    console.log(`${process.env.NEXT_PUBLIC_API_URL}?page=${page}&limit=${limit}`);
     const res = await axios.get(
         `${process.env.NEXT_PUBLIC_API_URL}?page=${page}&limit=${limit}`
     );
-    console.log(res.data);
     return res.data;
 };
 
